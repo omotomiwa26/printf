a27dd3505e9c4d167701e1a93eb7445d4ad0e3c7
saminstein <saminstein@yahoo.com>
2023-03-25 09:00:50 -0700

    prototypes for functions

--- main.h
+++ main.h
@@ -3,28 +3,55 @@
 
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <stdio.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
 
 
 /**
- * struct convert - defines a structure for symbols and function
- * @sym: The operator
- * @f: The function involved
+ * struct fmt - defines a structure for symbols and function
+ * @fmt: The format
+ * @fn: The function involved
  */
-struct convert
+struct fmt
 {
-	char *sym;
-	int (*f)(va_list);
-}
-typedef struct convert conver_t;
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
 
 
-/* main functions */
-int parser(const char *format, conver_t f_list[], va_list arg_list);
+/**
+ * typedef struct fmt fmt_t - Struct op
+ * @fmt: The format.
+ * @fm_t: The function associated
+ */
+typedef struct fmt fmt_t;
+
+
 int _printf(const char *format, ...);
-int _writechar(char c);
-int print_char(va_list);
-int print_string(va_list);
+int handle_print(const char *fmt, int, va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
-#endif
+/* functions */
+
+/* functions to print char and strings */
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+/* functions to print numbers */
+int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
