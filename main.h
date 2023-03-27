bbf36b87ab9088bc85d056128494086a558b51c0
saminstein <saminstein@yahoo.com>
2023-03-27 00:17:38 -0700

    What one has not experienced, one will never understand in print

--- main.h
+++ main.h
@@ -55,3 +55,8 @@ int print_percent(va_list types, char buffer[], int flags, int width, int precis
 int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
